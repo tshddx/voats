f5ad40e7b72e029031d4f92c83b21ea045a079d3
Thomas Shaddox <wediddoit@gmail.com>
2012-07-13 15:40:29 -0700

    Some validations.

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,6 +1,8 @@
 class Poll < ActiveRecord::Base
-  attr_accessible :user_id
+  attr_accessible :user_id, :user
 
   has_many :poll_options
   belongs_to :user
+
+  validates :user_id, :votes_per_user, :user_id, :presence => true
 end
--- app/models/poll_option.rb
+++ app/models/poll_option.rb
@@ -1,7 +1,8 @@
 class PollOption < ActiveRecord::Base
-  attr_accessible :description, :poll_id, :title, :user_id
+  attr_accessible :description, :poll_id, :title, :user_id, :poll, :user
 
   belongs_to :poll
   belongs_to :user
-
+  
+  validates :poll_id, :user_id, :presence => true
 end
