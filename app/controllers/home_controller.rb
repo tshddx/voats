8c4e1ed29e6b8a6496409e9118490a99e8e58c24
Thomas Shaddox <wediddoit@gmail.com>
2012-07-13 14:30:41 -0700

    Robohash for users.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    render :text => @user.present? ? @user.cookie : "No user"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,11 @@ class User < ActiveRecord::Base
   before_create do
     self.cookie = UUIDTools::UUID.random_create.to_s
   end
+
+  def robohash
+    # 1 or 2
+    set = (self.id % 2) + 1
+    return "http://robohash.org/#{self.cookie}.png?set=set#{set}"
+  end
   
 end
