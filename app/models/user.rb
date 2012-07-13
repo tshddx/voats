88507c7e3866f40dc838976137cb1e506b1e0bf1
Thomas Shaddox <wediddoit@gmail.com>
2012-07-13 14:49:17 -0700

    Basic list of polls.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ActiveRecord::Base
     self.cookie = UUIDTools::UUID.random_create.to_s
   end
 
-  def robohash
+  def robohash(size=200)
     # 1 or 2
     set = (self.id % 2) + 1
-    return "http://robohash.org/#{self.cookie}.png?set=set#{set}"
+    return "http://robohash.org/#{self.cookie}.png?set=set#{set}&size=#{size}x#{size}"
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Voats::Application.routes.draw do
   root :to => "home#index"
 
+  resources :polls
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
